4c9331bdd87dff65cfbf065c57d6a35f912e4266
Amir Hossein Fattahi <amixsty@gmail.com>
2022-07-01 02:19:28 +0430

    Project Files

---
+++ Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace MatchingGameAmixsty
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblstatus = new System.Windows.Forms.Label();
+            this.lbltimeleft = new System.Windows.Forms.Label();
+            this.btnrestart = new System.Windows.Forms.Button();
+            this.gametimer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblstatus
+            // 
+            this.lblstatus.AutoSize = true;
+            this.lblstatus.Location = new System.Drawing.Point(317, 123);
+            this.lblstatus.Name = "lblstatus";
+            this.lblstatus.Size = new System.Drawing.Size(13, 20);
+            this.lblstatus.TabIndex = 0;
+            this.lblstatus.Text = " ";
+            // 
+            // lbltimeleft
+            // 
+            this.lbltimeleft.AutoSize = true;
+            this.lbltimeleft.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbltimeleft.Location = new System.Drawing.Point(317, 215);
+            this.lbltimeleft.Name = "lbltimeleft";
+            this.lbltimeleft.Size = new System.Drawing.Size(23, 34);
+            this.lbltimeleft.TabIndex = 1;
+            this.lbltimeleft.Text = " ";
+            // 
+            // btnrestart
+            // 
+            this.btnrestart.BackColor = System.Drawing.Color.Orange;
+            this.btnrestart.Font = new System.Drawing.Font("Showcard Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnrestart.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.btnrestart.Location = new System.Drawing.Point(254, 48);
+            this.btnrestart.Name = "btnrestart";
+            this.btnrestart.Size = new System.Drawing.Size(133, 38);
+            this.btnrestart.TabIndex = 2;
+            this.btnrestart.Text = "New Game !";
+            this.btnrestart.UseVisualStyleBackColor = false;
+            this.btnrestart.Click += new System.EventHandler(this.RestartGameEvent);
+            // 
+            // gametimer
+            // 
+            this.gametimer.Interval = 1000;
+            this.gametimer.Tick += new System.EventHandler(this.TimerEvent);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(273, 177);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Times Left:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(412, 333);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnrestart);
+            this.Controls.Add(this.lbltimeleft);
+            this.Controls.Add(this.lblstatus);
+            this.Name = "Form1";
+            this.Text = "Matching Game Amixsty";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblstatus;
+        private System.Windows.Forms.Label lbltimeleft;
+        private System.Windows.Forms.Button btnrestart;
+        private System.Windows.Forms.Timer gametimer;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MatchingGameAmixsty
+{
+    public partial class Form1 : Form
+    {
+        List<int> numbers = new List<int> { 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6 };
+        string firstChoise;
+        string secondChoise;
+        int treis;
+        List<PictureBox> pictures = new List<PictureBox>();
+        PictureBox picA;
+        PictureBox picB;
+        int totaltime = 30;
+        int countDownTime;
+        bool gameOver = false;
+        public Form1()
+        {
+            InitializeComponent();
+            LoadPictures();
+        }
+
+        private void TimerEvent(object sender, EventArgs e)
+        {
+
+            countDownTime--;
+            lbltimeleft.Text = "" + countDownTime;
+            if (countDownTime < 10 && countDownTime > 1)
+            {
+                lbltimeleft.ForeColor = Color.Red;
+            }
+                if (countDownTime < 1)
+            {
+                GameOver("times up , you loose");
+                foreach(PictureBox x in pictures)
+                {
+                    if (x.Tag != null)
+                    {
+                        x.Image = Image.FromFile("matchgamepics/" + (string)x.Tag + ".png");
+                    }
+                }
+            }
+        }
+
+
+        private void RestartGameEvent(object sender, EventArgs e)
+        {
+            RestartGame();
+        }
+        private void LoadPictures()
+        {
+            int leftPos = 20;
+            int topPos = 20;
+            int rows = 0;
+            for (int i = 0; i < 12; i++)
+            {
+                PictureBox newPic = new PictureBox();
+                newPic.Height = 50;
+                newPic.Width = 50;
+                newPic.BackColor = Color.LightSkyBlue;
+                newPic.SizeMode = PictureBoxSizeMode.StretchImage;
+                newPic.Click += NewPic_Click;
+                pictures.Add(newPic);
+
+                if (rows < 3)
+                {
+                    rows++;
+                    newPic.Left = leftPos;
+                    newPic.Top = topPos;
+                    this.Controls.Add(newPic);
+                    leftPos = leftPos + 60;
+                }
+                if (rows == 3)
+                {
+                    leftPos = 20;
+                    topPos += 60;
+                    rows = 0;
+                }
+            }
+            RestartGame();
+        }
+
+        private void NewPic_Click(object sender, EventArgs e)
+        {
+            if (gameOver)
+            {
+                return;
+            }
+            if (firstChoise == null)
+            {
+                picA = sender as PictureBox;
+                if (picA.Tag != null && picA.Image == null)
+                {
+                    picA.Image = Image.FromFile("matchgamepics/"+(string)picA.Tag + ".png");
+                    firstChoise = (string)picA.Tag;
+
+                }
+     
+            }
+            else if (secondChoise == null)
+            {
+                picB = sender as PictureBox;
+                if (picB.Tag != null && picB.Image ==null)
+                {
+                    picB.Image = Image.FromFile("matchgamepics/" + (string)picB.Tag + ".png");
+                    secondChoise = (string)picB.Tag;
+                }
+            }
+            else
+            {
+                CheckPictures(picA, picB);
+            }
+        }
+
+        private void RestartGame()
+        {
+            lbltimeleft.ForeColor = Color.Black;
+            var randomList = numbers.OrderBy(x => Guid.NewGuid()).ToList();
+            numbers = randomList;
+
+            for (int i = 0; i < pictures.Count; i++)
+            {
+                pictures[i].Image = null;
+                pictures[i].Tag = numbers[i].ToString();
+
+            }
+            treis = 0;
+            lblstatus.Text = "Wrong " + treis ;
+            lbltimeleft.Text = "" + totaltime;
+            gameOver = false;
+            gametimer.Start();
+            countDownTime = totaltime;
+
+
+        }
+
+
+        private void CheckPictures(PictureBox A , PictureBox B)
+        {
+            if (firstChoise == secondChoise)
+            {
+                A.Tag = null;
+                B.Tag = null;
+            }
+            else
+            {
+                treis++;
+                lblstatus.Text = "Wrong " + treis ;
+
+            }
+            firstChoise = null;
+            secondChoise = null;
+
+            foreach(PictureBox pics in pictures.ToList())
+            {
+                if (pics.Tag != null)
+                {
+                    pics.Image = null;
+                }
+            }
+
+            if (pictures.All(o => o.Tag == pictures[0].Tag))
+            {
+                GameOver("Great Work!");
+            }
+
+
+
+        }
+
+        private void GameOver(string msg)
+        {
+            gametimer.Stop();
+            gameOver = true;
+            MessageBox.Show(msg + " click restart to play again","game msg");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
